282bc85d572f1bb53b78f7e16674e1f1bf3c921b
KInkH <llopezberist@gmail.com>
2025-01-04 00:01:20 -0600

    First version of a self generated labyrinth.

--- Assets/WallController.cs
+++ Assets/WallController.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
 
-public class MainController : MonoBehaviour
+/*
+ * Wall Controller Class
+ * Author: Liliana Lopez Beristain
+ * This class instantiates a random labyrinth from a predefined wall block.
+ */
+public class WallController : MonoBehaviour
 {
-    //Global Variables
+    /* Private variables */
     private int[] labyrinth;
     private int wallsNumber   = 2;
     private int totalDoors    = 8;
+
+    /* Public Serialized Variables */
     public char rotation; // Y or N
     public GameObject wall;
 
@@ -13,23 +20,19 @@ public class MainController : MonoBehaviour
     {
         int i;
         int selectedWall;
-        //Initialize variables
+       
+        // Initialize variables
         labyrinth = new int[totalDoors];
 
-        // We create the labyrinth
-        for ( i = 0; i < wallsNumber; i++)
+        // Create the labyrinth
+        for (i = 0; i < wallsNumber; i++)
         {
-            selectedWall = getRandom(i * 4, i * 4 + 3);
+            selectedWall = getRandom(i * wallsNumber, i * wallsNumber + wallsNumber-1);
             labyrinth[selectedWall] = 1;
         }
 
         createLabyrinth(labyrinth);
-    }
-
-    void Update()
-    {
-        
-    }
+    }  
 
     /* Generates a random labyrinth */
     private void createLabyrinth(int[] labyrinth)
@@ -42,16 +45,17 @@ public class MainController : MonoBehaviour
 
         wallWidth = wall.GetComponent<Collider>().bounds.size;
 
-        for ( i = 0; i < totalDoors; i++ )
+        for (i = 0; i < totalDoors; i++ )
         {
-            if ( labyrinth[i] == 0)
+            if (labyrinth[i] == 0)
             {
                 if( rotation == 'Y' )
                 {
                     newXCoordinates = transform.position.z + (wallWidth.z * i);
                     currentPosition = new Vector3(transform.position.x, transform.position.y, newXCoordinates);
                     currentRotation = Quaternion.Euler(0, 90, 0);
-                } else
+                } 
+                else
                 {
                     newXCoordinates = transform.position.x - (wallWidth.x * i);
                     currentPosition = new Vector3(newXCoordinates, transform.position.y, transform.position.z);
